0050e267b95aa3dfcb21b599760021d1b58eb47f
RRonniecool <000ismailmubarak@gmail.com>
2024-12-31 11:19:26 +0100

    increased the load time to 8hrs

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,28 +1,31 @@
-import React, { useState, useEffect } from 'react'; 
-import Header from '../components/Header/Header';
-import Footer from '../components/Footer/Footer';
-import Appointment from '../components/Appointment/Appointment';
-import Carousel from '../components/Carousel/Carousel'
-import Aboutus from '../components/AboutUs/AboutUs'
-import Gallery from '../components/Gallery/Gallery'
-import './homepage.css';
+import React, { useState, useEffect } from "react";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import Appointment from "../components/Appointment/Appointment";
+import Carousel from "../components/Carousel/Carousel";
+import Aboutus from "../components/AboutUs/AboutUs";
+import Gallery from "../components/Gallery/Gallery";
+import "./homepage.css";
 
 const HomePage = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setIsLoading(false); 
-    }, 8000);
+      setIsLoading(false);
+    }, 28800000);
 
-    return () => clearTimeout(timer); 
+    return () => clearTimeout(timer);
   }, []);
 
   return (
     <div>
       {isLoading ? (
         <div className="loader">
-          <img src={`${process.env.PUBLIC_URL}/book-loader.gif`} alt="Loading..." />
+          <img
+            src={`${process.env.PUBLIC_URL}/book-loader.gif`}
+            alt="Loading..."
+          />
         </div>
       ) : (
         <div>
